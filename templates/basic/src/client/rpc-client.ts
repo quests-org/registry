8f39ebc8691a7b87b6a931c9378ddcc131b2dfda
Jeremy Mack <mutewinter@users.noreply.github.com>
2025-09-02 08:25:57 -0500

    chore(basic): remove unused exports

--- templates/basic/src/client/rpc-client.ts
+++ templates/basic/src/client/rpc-client.ts
@@ -1,8 +1,4 @@
-import type {
-  InferRouterInputs,
-  InferRouterOutputs,
-  RouterClient,
-} from "@orpc/server";
+import type { RouterClient } from "@orpc/server";
 import { createORPCClient } from "@orpc/client";
 import { RPCLink } from "@orpc/client/fetch";
 import { createTanstackQueryUtils } from "@orpc/tanstack-query";
@@ -14,7 +10,3 @@ const link = new RPCLink({ url: `${window.location.origin}/rpc` });
 export const rpcClient: RouterClient<typeof router> = createORPCClient(link);
 
 export const queryClient = createTanstackQueryUtils(rpcClient);
-
-// Use to access the typed inputs and outputs of the server
-export type Outputs = InferRouterOutputs<typeof router>;
-export type Inputs = InferRouterInputs<typeof router>;
